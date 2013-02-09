37b0887c1ae7a923a0c1f4c7155086fe40c4c2a9
David Hazinski <dhazinski@gmail.com>
2013-02-08 20:28:23 -0500

    Creating debug environment variable.

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ def camelcase(s):
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    debug = os.environ.get('DEBUG', False)
+    app.run(host='0.0.0.0', port=port, debug=debug)
